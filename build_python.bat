4ce0d16cd7feff77bc6e27aae62d940779678a1c
adambonneruk <adam@bonner.uk>
2020-09-03 13:11:08 +0100

    working build .bat

--- build_python.bat
+++ build_python.bat
@@ -9,13 +9,4 @@ REM Fix Launch as Admin Directory Problem
 
 pyinstaller.exe --onefile --icon=./icon/icon.ico TransposeCSV.py
 
-MOVE "dist\TransposeCSV.exe" "..\" >nul
-
-DEL TransposeCSV.spec >nul
-DEL TransposeCSV.py >nul
-RMDIR __pycache__ /S /Q
-RMDIR build /S /Q
-RMDIR dist /S /Q
-
 ECHO.
-PAUSE
