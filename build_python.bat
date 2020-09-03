56d36ef5233fd98e69562c889800ddef14d88128
adambonneruk <adam@bonner.uk>
2020-09-03 13:23:33 +0100

    simplified the bat file

--- build_python.bat
+++ build_python.bat
@@ -1,12 +1,1 @@
-REM call lib\build_python.bat TransposeCSV
-COLOR 0B
-CLS
-@ECHO OFF
-
-REM Fix Launch as Admin Directory Problem
-@setlocal enableextensions
-@cd /d "%~dp0"
-
 pyinstaller.exe --onefile --icon=./icon/icon.ico TransposeCSV.py
-
-ECHO.
